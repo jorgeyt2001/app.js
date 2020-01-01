2ae01340188a85f3a0b18ae0099edb32ce8882ec
jorgeyt2001 <48733671+jorgeyt2001@users.noreply.github.com>
2020-01-01 23:58:18 +0100

    Delete r6stats.js

--- commands/r6stats.js
+++
@@ -1,8 +0,0 @@
-const R6API = require('r6api.js');
-
- 
-const username = 'corito_4_.free';
-const id = await r6.getId('uplay', username).then(el => el[0].id);
-const stats = await r6.getStats('uplay', id).then(el => el[0]);
- 
-console.log(`${username} has played ${stats.pvp.general.matches} matches`);
